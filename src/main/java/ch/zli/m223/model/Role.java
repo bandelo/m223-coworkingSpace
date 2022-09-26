20b6ace506ef44d3723439a2561b2c7b07fff96c
bandelo <andelo.batinic@lernende.bbw.ch>
2022-09-26 12:08:52 +0000

    feat(entity): implemented model for role

---
+++ src/main/java/ch/zli/m223/model/Role.java
@@ -0,0 +1,41 @@
+package ch.zli.m223.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/*
+ * @author Andelo Batinic
+ * @date 26.09 2022
+ * This is the model for the Model
+ */
+
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
