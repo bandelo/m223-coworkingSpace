d666ad10ad24ab2213e57d9b38776d9991981467
bandelo <andelo.batinic@lernende.bbw.ch>
2022-09-26 12:23:11 +0000

    feat(entity): implemented model for bereiche

---
+++ src/main/java/ch/zli/m223/model/Bereiche.java
@@ -0,0 +1,52 @@
+package ch.zli.m223.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/*
+ * @author Andelo Batinic
+ * @date 26.09 2022
+ * This is the model for the Bereiche
+ */
+
+public class Bereiche {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+}
